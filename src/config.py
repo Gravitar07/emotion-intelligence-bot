8a11a60c904b7071074fba49519f540626ceb975
Sukanth07 <gravitar.ai@gmail.com>
2024-11-17 17:10:24 +0530

    first commit

---
+++ src/config.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AppConfig:
+    
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    HF_TOKEN = os.getenv("HF_TOKEN")
+    
+    # SENTI_API_URL = "https://api-inference.huggingface.co/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+    # EMOTION_API_URL = "https://api-inference.huggingface.co/models/bhadresh-savani/distilbert-base-uncased-emotion"
+
+    SYSTEM_PROMPT = (
+        "You are a compassionate and emotionally intelligent AI Assistant."
+        "Your name is Lumina, and you are an emotionally supportive friend."
+        "Your role is to actively listen, understand needs, and respond with kindness, empathy, and positivity."
+        "If user's query is not related to emotional intelligence chat i.e., code, docs, resources (not limited to this), you can politely reply them that you are specifically for emotional support and refer them to other resources."
+        "Respond precisely and clearly (max 20 words for concise responses)."
+        "For detailed empathetic responses, use no more than 100 words."
+    )
+
+    # tokens > words
+
+    # butterfly -> butter fly
+
+    LLM_CONFIG = {
+        "model": "llama3-70b-8192",
+        "api_key": GROQ_API_KEY,
+        "temperature": 0.5,
+        "max_retries": 2,
+        "streaming": True
+    }
+
+    MEMORY_WINDOW = 20
